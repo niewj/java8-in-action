50a826e0ea933834dfd5f6743bdff948923e8010
weijun.nie <weijun.nie@msxf.com>
2017-10-12 13:24:05 +0800

    oh,add multi-patterns

---
+++ ext01-multithreads/src/main/java/com/niewj/basic/multi/patterns/immutable/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * 不变模式
+ * Created by weijun.nie on 2017/10/12.
+ */
+package com.niewj.basic.multi.patterns.immutable;
---
+++ ext01-multithreads/src/main/java/com/niewj/basic/multi/patterns/singleton/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * <url>http://www.jianshu.com/p/37dc399eed27</url>
+ * Created by weijun.nie on 2017/10/12.
+ */
+package com.niewj.basic.multi.patterns.singleton;
